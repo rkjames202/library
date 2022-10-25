b3159229efac1123ba06b9c0f54397f557c7c977
Russell James <russelljames202@gmail.com>
2022-10-25 11:00:10 -0400

    Refactor and change multiple methods
    Removed placeholder books in library array
    
    Book() constructor now trims the whitespace from the title and author
    fields.
    
    addBookToLibrary() stores the value of the input fields into variables
    instead of referencing nodes
    
    renderTable() only displays table if library array is not empty.
    
    changeStatus() no longer loops through the library array but instead
    uses the 'data-book-number' attribute's value as the index of the book
    object. Makes more sense this way.
    
    fadeOutModal() now clears the input fields.

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-let book1 = new Book("Lord", "P", 23, false);
-let book2 = new Book("Lord", "P", 22, false);
-let book3 = new Book("Lord", "P", 21, false);
-
-let myLibrary = [book1, book2, book3];
+let myLibrary = [];
 
 function main(){
     renderTable();
@@ -11,8 +7,8 @@ function main(){
 
 /**constructor */
 function Book(title, author, pages, status){
-    this.title = title;
-    this.author = author;
+    this.title = title.trim();
+    this.author = author.trim();
     this.pages = pages;
     this.status = status;
 
@@ -32,6 +28,7 @@ function Book(title, author, pages, status){
         
         statusButton.innerText = `${this.status ? "Read": "Not Read"}`;
         removeButton.innerText = "Remove";
+        statusButton.style.backgroundColor = `${this.status ? "var(--table-button-green)": "var(--table-button-red)"}`
 
         statusButton.addEventListener('click', changeStatus);
         removeButton.addEventListener('click', removeBook);
@@ -49,25 +46,18 @@ function Book(title, author, pages, status){
 
 function addBookToLibrary(){
     const form = document.querySelector(".modal-form form");
-    const title = document.querySelector("#title");
-    const author = document.querySelector("#author");
-    const pages = document.querySelector("#pages");
-    const status = document.querySelector("#book-status");
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const status = document.querySelector("#book-status").checked;
 
     if(form.checkValidity()){
         const bookTable = document.querySelector("table");
-        let newBook = new Book(title.value, author.value, pages.value, status.checked);
-        myLibrary.push(newBook);
+        let newBook = new Book(title, author, pages, status);
+        myLibrary.unshift(newBook);
 
         fadeOutModal();
-        setTimeout(() =>{
-            title.value = "";
-            author.value = "";
-            pages.value = "";
-            status.checked = false;
-        }, 500);
-
-       
+      
         bookTable.appendChild(newBook.createRow()); 
         renderTable();
 
@@ -88,6 +78,13 @@ function renderTable(){
     myLibrary.forEach((book) => {
         bookTable.appendChild(book.createRow());   
     });
+
+    //Only display table if library isn't empty
+    if(myLibrary.length == 0){
+        bookTable.style.display = "none";
+    } else {
+        bookTable.style.display = "block";
+    }
     
 }
 
@@ -108,14 +105,12 @@ function removeBook(){
 function changeStatus(){
     //Get the grandparent of the status button, the <tr> element
     const row = this.parentNode.parentNode;
-
-    myLibrary.forEach((book, index) => {       
-        if(row.getAttribute("data-book-number") == index){
-            book.status = !book.status; //change status to opposite of current status
-            this.innerText = `${book.status ? "Read" : "Not Read"}`; //change button' inner text
-        }
-    });
-    
+    let book = myLibrary[row.getAttribute("data-book-number")];
+   
+    book.status = !book.status; //change status to opposite of current status
+    this.innerText = `${book.status ? "Read" : "Not Read"}`; //change button' inner text
+    this.style.backgroundColor = `${book.status ? "var(--table-button-green)" : "var(--table-button-red)"}`;
+   
 }
 
 function addEventListeners(){
@@ -138,20 +133,29 @@ function addEventListeners(){
     });
 }
 
-
 function fadeInModal(){
     const modal = document.querySelector(".modal");
-
+  
     modal.style.animation = "fadeIn .5s";
     modal.style.display = "block";
+
 }
 
 function fadeOutModal(){
     const modal = document.querySelector(".modal");
+    const title = document.querySelector("#title");
+    const author = document.querySelector("#author");
+    const pages = document.querySelector("#pages");
+    const status = document.querySelector("#book-status");
 
     modal.style.animation = "fadeOut .5s";
+    
     setTimeout(() =>{
         modal.style.display = "none";
+        title.value = "";
+        author.value = "";
+        pages.value = "";
+        status.checked = false;
     }, 400);
 }
 
@@ -169,9 +173,8 @@ function resetBookButton(){
 
 /**
  * Commit
- * Style each section one at a time
- * dont forget about form
- * Add commits along the way
+ * Refactor script if needed, commit
+ * Add comments to script, commit
  * push project
  */
 
